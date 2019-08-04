2f0717c539f0a166a4d9070e9f08eee62cee9551
Francisco <Francjaxi1@gmail.com>
2019-08-04 16:19:28 -0500

    Proyecto inicial

---
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1,et2;
+    private TextView tv3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1=(EditText)findViewById(R.id.etPrimer);
+        et2=(EditText)findViewById(R.id.etSegundo);
+        tv3=(TextView)findViewById(R.id.tvResultado);
+    }
+
+    public void sumar(View view) {
+        String valor1=et1.getText().toString();
+        String valor2=et2.getText().toString();
+        int nro1=Integer.parseInt(valor1);
+        int nro2=Integer.parseInt(valor2);
+        int suma=nro1+nro2;
+        String resu=String.valueOf(suma);
+        tv3.setText(resu);
+    }
+    public void restar(View view) {
+        String valor1=et1.getText().toString();
+        String valor2=et2.getText().toString();
+        int nro1=Integer.parseInt(valor1);
+        int nro2=Integer.parseInt(valor2);
+        int suma=nro1-nro2;
+        String resu=String.valueOf(suma);
+        tv3.setText(resu);
+    }
+    public void multiplicar(View view) {
+        String valor1=et1.getText().toString();
+        String valor2=et2.getText().toString();
+        int nro1=Integer.parseInt(valor1);
+        int nro2=Integer.parseInt(valor2);
+        int suma=nro1*nro2;
+        String resu=String.valueOf(suma);
+        tv3.setText(resu);
+    }
+    public void dividir(View view) {
+        String valor1=et1.getText().toString();
+        String valor2=et2.getText().toString();
+        int nro1=Integer.parseInt(valor1);
+        int nro2=Integer.parseInt(valor2);
+        float suma=(float) nro1/nro2;
+        String resu=String.valueOf(suma);
+        tv3.setText(resu);
+    }
+}
